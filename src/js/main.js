17fb81e9507261c14a658e54a52ada74a29315b3
Клевакин Артем Николаевич <themightymasha@gmail.com>
2021-01-11 19:18:57 +0500

    added tabs.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import './slider';
 import modals from './Modules/modals'
+import tabs from './Modules/tabs'
 
 
 window.addEventListener('DOMContentLoaded', () => {
     modals()
+    tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    //(headerSelector, tabSelector, contentSelector, activeClass)
 })
